5206841ae751b4b28675e1ddb64f7085df4e6cdd
Hellore93 <sztorcu18@gmail.com>
2024-11-04 14:45:21 +0100

    Added datatable and new material

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,16 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { ProductService } from "../../services/queryService";
+import { DataGrid } from '@mui/x-data-grid';
 
 export const Home = () => {
+    const [rows, setRows] = useState(null);
 
     useEffect(() => {
         const products = ProductService.getAllProducts();
         console.log('Products >> ', products);
+        setRows(products);
       }, []);
+
+    const columns = [
+        { field: 'id', headerName: 'ID', width: 70 },
+        { field: 'Name', headerName: 'Name', width: 70 },
+        { field: 'Quantity', headerName: 'Quantity', width: 70 },
+    ];
+
+    const paginationModel = { page: 0, pageSize: 5 };
 
     return (
         <div>
-            <p>Test czy działa</p>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                initialState={{ pagination: { paginationModel } }}
+                pageSizeOptions={[5, 10]}
+                checkboxSelection
+                sx={{ border: 0 }}
+            />
         </div>
     )
 }
