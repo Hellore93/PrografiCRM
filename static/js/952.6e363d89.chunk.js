6edbc6926a9eb4c91401a8cbd78222221534352c
Hellore93 <Hellore93@users.noreply.github.com>
2024-11-04 08:21:31 +0000

    deploy: 1d867cf46a202220e62eb1e6d3f4d47d33682841

---
+++ static/js/952.6e363d89.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkprografi=self.webpackChunkprografi||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.6e363d89.chunk.js.map
